44a3dd5fe19a6fcb715cdd2a9e6969253d6679de
elhendricks <elhendricks@gmail.com>
2016-09-30 16:40:18 -0700

    Refactored word frequency function to account for all projects

--- scripts/Views/projectView.js
+++ scripts/Views/projectView.js
@@ -42,22 +42,43 @@
 
     });
   };
+
   projectViews.wordFrequency = function() {
   // THIS WORKS, BUT I THINK I CAN CLEAN IT UP BY CHAINING METHODS
-    var words = Project.all[0].body.split(' ');
-    //console.log(words);
+    var words = []
+    // Project.all[0].body.split(' ');
+    // console.log(words);
+    //
+    // .forEach(function(project) {
+    //   return project.body.split(' ');
+    // });
+
+    Project.all.forEach(function(item) {
+      words.push(item.body.split(' '));
+    });
+
+    console.log(words);
+
+    words = words.reduce(function(acc, curr){
+      acc = acc.concat(curr);
+      return acc;
+    });
+
+    console.log("after concat " + words);
+
+
     var uniqueWords =  words.reduce(function(acc, curr) {
       acc[curr] = 0;
       return acc;
     },{});
-    //console.log(uniqueWords);
+    console.log(uniqueWords);
     words.forEach(function(w){
       if (w in uniqueWords) {
         uniqueWords[w] += 1;
       //  console.log(w);
       }
     });
-  //  console.log(uniqueWords);
+   console.log(uniqueWords);
     return uniqueWords;
 
   };
@@ -66,7 +87,7 @@
     var highFrequency = [];
     //console.log(wordFrequency);
     for (var key in wordFrequency){
-      if (wordFrequency[key] >= 2) {
+      if (wordFrequency[key] >= 6) {
         highFrequency.push(key);
       }
     }
@@ -77,21 +98,21 @@
     console.log(highFrequency);
   };
 
-  projectViews.wordCount = function() {
-    return Project.all.map(function(article) {
-      return article.body.match(/\w+/g).length;
-    }).reduce(function(acc, curr){
-      return acc + curr;
-    });
-  };
-  projectViews.initWordCount = function() {
-    $('#words').text(projectViews.wordCount());
-  };
+  // projectViews.wordCount = function() {
+  //   return Project.all.map(function(article) {
+  //     return article.body.match(/\w+/g).length;
+  //   }).reduce(function(acc, curr){
+  //     return acc + curr;
+  //   });
+  // };
+  // projectViews.initWordCount = function() {
+  //   $('#words').text(projectViews.wordCount());
+  // };
 
 
   module.projectViews = projectViews;
   Project.fetchAll(projectViews.renderIndexPage);
-  projectViews.initWordCount();
+  // projectViews.initWordCount();
   projectViews.initWordFrequency(projectViews.wordFrequency());
   projectViews.handleCategoryFilter();
 
