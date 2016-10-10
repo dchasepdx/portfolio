22ba4cbe1f64a52c70b8ce9b5bce81218c5925da
dchasepdx <dchasepdx@gmail.com>
2016-10-09 19:00:11 -0700

    changed the threshold for high frequency words

--- scripts/Views/projectView.js
+++ scripts/Views/projectView.js
@@ -2,21 +2,6 @@
 (function(module) {
   var projectViews = {};
 
-  // projectViews.aboutOrProjects = function () {
-  //
-  //   $('nav').on('click', 'ul li:lt(2)',function(){
-  //     $('#about').hide();
-  //     $('#projects').hide();
-  //     var $text = $(this).text().toLowerCase();
-  //     $('#' + $text).show();
-  //     if ($text === 'about') {
-  //       $('#category-filter').hide();
-  //     } else {
-  //       $('#category-filter').show();
-  //     }
-  //
-  //   });
-  // };
 
   projectViews.renderIndexPage = function() {
     Project.all.forEach(function(a){
@@ -27,7 +12,7 @@
       $('#about').hide();
     });
 
-    // projectViews.aboutOrProjects();
+
 
   };
 
@@ -44,51 +29,40 @@
   };
 
   projectViews.wordFrequency = function() {
-  // THIS WORKS, BUT I THINK I CAN CLEAN IT UP BY CHAINING METHODS
     var words = [];
-    // Project.all[0].body.split(' ');
-    // console.log(words);
-    //
-    // .forEach(function(project) {
-    //   return project.body.split(' ');
-    // });
+
 
     Project.all.forEach(function(item) {
       words.push(item.body.split(' '));
 
     });
 
-  //  console.log(words);
+
 
     words = words.reduce(function(acc, curr){
       acc = acc.concat(curr);
       return acc;
     });
 
-    //console.log('after concat ' + words);
 
 
     var uniqueWords =  words.reduce(function(acc, curr) {
       acc[curr] = 0;
       return acc;
     },{});
-    //console.log(uniqueWords);
     words.forEach(function(w){
       if (w in uniqueWords) {
         uniqueWords[w] += 1;
-      //  console.log(w);
       }
     });
-    //console.log(uniqueWords);
     return uniqueWords;
 
   };
 
   projectViews.initWordFrequency = function(wordFrequency) {
     var highFrequency = [];
-    //console.log(wordFrequency);
     for (var key in wordFrequency){
-      if (wordFrequency[key] >= 20) {
+      if (wordFrequency[key] >= 2) {
         highFrequency.push(key);
       }
     }
@@ -96,24 +70,12 @@
       return acc + ', ' + curr;
     });
     $('#most-used-words').append('<span>' + mostUsedWords + '</span>');
-    //console.log(highFrequency);
   };
 
-  // projectViews.wordCount = function() {
-  //   return Project.all.map(function(article) {
-  //     return article.body.match(/\w+/g).length;
-  //   }).reduce(function(acc, curr){
-  //     return acc + curr;
-  //   });
-  // };
-  // projectViews.initWordCount = function() {
-  //   $('#words').text(projectViews.wordCount());
-  // };
 
 
   module.projectViews = projectViews;
   Project.fetchAll(projectViews.renderIndexPage);
-  // projectViews.initWordCount();
   projectViews.initWordFrequency(projectViews.wordFrequency());
   projectViews.handleCategoryFilter();
 
